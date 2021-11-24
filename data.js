9dbae9a7688e01be74561f07906f2272aff130bb
Caelian-A <92312529+Caelian-A@users.noreply.github.com>
2021-11-24 13:45:36 +0000

    bondMovies object moved to data.js

---
+++ data.js
@@ -0,0 +1,156 @@
+let bondMovies = [
+    {
+        title: 'Dr. No',
+        director: ['Terence Young'],
+        bondActor: ['Sean Connery'],
+        year: 1962
+    },
+    {
+        title: 'From Russia with Love',
+        director: ['Terence Young'],
+        bondActor: ['Sean Connery'],
+        year: 1963
+    },
+    {
+        title: 'Goldfinger',
+        director: ['Guy Hamilton'],
+        bondActor: ['Sean Connery'],
+        year: 1964
+    },
+    {
+        title: 'Thunderball',
+        director: ['Terence Young'],
+        bondActor: ['Sean Connery'],
+        year: 1965
+    },
+    {
+        title: 'You Only Live Twice',
+        director: ['Lewis Gilbert'],
+        bondActor: ['Sean Connery'],
+        year: 1967
+    },
+    {
+        title: 'On Her Majesty\'s Secret Service',
+        director: ['Peter Roger Hunt'],
+        bondActor: ['George Lazenby'],
+        year: 1969
+    },
+    {
+        title: 'Diamonds Are Forever',
+        director: ['Guy Hamilton'],
+        bondActor: ['Sean Connery'],
+        year: 1971
+    },
+    {
+        title: 'Live and Let Die',
+        director: ['Guy Hamilton'],
+        bondActor: ['Roger Moore'],
+        year: 1973
+    },
+    {
+        title: 'The Man with the Golden Gun',
+        director: ['Guy Hamilton'],
+        bondActor: ['Roger Moore'],
+        year: 1974
+    },
+    {
+        title: 'The Spy Who Loved Me',
+        director: ['Lewis Gilbert'],
+        bondActor: ['Roger Moore'],
+        year: 1977
+    },
+    {
+        title: 'Moonraker',
+        director: ['Lewis Gilbert'],
+        bondActor: ['Roger Moore'],
+        year: 1979
+    },
+    {
+        title: 'For Your Eyes Only',
+        director: ['John Glen'],
+        bondActor: ['Roger Moore'],
+        year: 1981
+    },
+    {
+        title: 'Octopussy',
+        director: ['John Glen'],
+        bondActor: ['Roger Moore'],
+        year: 1983
+    },
+    {
+        title: 'A View to a Kill',
+        director: ['John Glen'],
+        bondActor: ['Roger Moore'],
+        year: 1985
+    },
+    {
+        title: 'The Living Daylights',
+        director: ['John Glen'],
+        bondActor: ['Timothy Dalton'],
+        year: 1987
+    },
+    {
+        title: 'Licence to Kill',
+        director: ['John Glen'],
+        bondActor: ['Timothy Dalton'],
+        year: 1989
+    },
+    {
+        title: 'GoldenEye',
+        director: ['Martin Campbell	'],
+        bondActor: ['Pierce Brosnan'],
+        year: 1995
+    },
+    {
+        title: 'Tomorrow Never Dies',
+        director: ['Roger Spottiswoode'],
+        bondActor: ['Pierce Brosnan'],
+        year: 1997
+    },
+    {
+        title: 'The World Is Not Enough',
+        director: ['Michael Apted'],
+        bondActor: ['Pierce Brosnan'],
+        year: 1999
+    },
+    {
+        title: 'Die Another Day',
+        director: ['Lee Tamahori'],
+        bondActor: ['Pierce Brosnan'],
+        year: 2002
+    },
+    {
+        title: 'Casino Royale',
+        director: ['Martin Campbell'],
+        bondActor: ['Daniel Craig'],
+        year: 2006
+    },
+    {
+        title: 'Quantum of Solace',
+        director: ['Marc Forster'],
+        bondActor: ['Daniel Craig'],
+        year: 2008
+    },
+    {
+        title: 'Skyfall',
+        director: ['Sam Mendes'],
+        bondActor: ['Daniel Craig'],
+        year: 2012
+    },
+    {
+        title: 'Spectre',
+        director: ['Sam Mendes'],
+        bondActor: ['Daniel Craig'],
+        year: 2015
+    },
+    {
+        title: 'No Time to Die',
+        director: ['Cary Joji Fukunaga'],
+        bondActor: ['Daniel Craig'],
+        year: 2021
+    }
+  ];
+
+  module.exports = {
+    bondMovies: bondMovies
+  }
